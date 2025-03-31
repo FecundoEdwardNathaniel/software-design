7f2ecf1480134ae495f5eebc578a668720eba153
FecundoEdwardNathaniel <fecundoedwardnathaniel@gmail.com>
2025-03-31 14:19:14 +0800

    added exercie 1

--- lab1/Form1.Designer.cs
+++ lab1/Form1.Designer.cs
@@ -20,20 +20,53 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBoxResults = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listBoxResults
+            // 
+            listBoxResults.FormattingEnabled = true;
+            listBoxResults.ItemHeight = 15;
+            listBoxResults.Location = new Point(308, 79);
+            listBoxResults.Name = "listBoxResults";
+            listBoxResults.Size = new Size(278, 289);
+            listBoxResults.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(67, 73);
+            button1.Name = "button1";
+            button1.Size = new Size(95, 35);
+            button1.TabIndex = 1;
+            button1.Text = "Sort";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btnSort;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(listBoxResults);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private ListBox listBoxResults;
+        private Button button1;
+    }
 }
--- lab1/Form1.cs
+++ lab1/Form1.cs
@@ -1,10 +1,27 @@
 namespace lab1
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int[] ProceduralSort(int[] numbers)
+        {
+            Array.Sort(numbers);
+            return numbers;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void btnSort(object sender, EventArgs e)
+        {
+            int[] numbers = { 3, 1, 5, 2, 4 };
+            int[] sortednumbers = ProceduralSort(numbers);
+            listBoxResults.DataSource = sortednumbers;
+        }
+    }
 }
